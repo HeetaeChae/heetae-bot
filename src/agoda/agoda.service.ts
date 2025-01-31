4a735c32ff631798355b62b5056c55780fedffa1
chaeheetae <chaeheetae94@gmail.com>
2025-01-31 16:59:51 +0900

    호텔 정보 가져오기 부분 디버깅

--- src/agoda/agoda.service.ts
+++ src/agoda/agoda.service.ts
@@ -23,6 +23,7 @@ export class AgodaService {
     return [startDate, endDate].map((day) => day.split('-')[2]);
   }
 
+  // 호텔 검색으로 이동
   async goToSearchPage(page: Page) {
     // 트립닷컴 이동
     await Promise.all([
@@ -38,6 +39,7 @@ export class AgodaService {
     await this.utilsService.delayRandomTime('slow');
   }
 
+  // 검색 처리
   async searchCity(page: Page, city: string) {
     // 도시명 타이핑
     await page.type('#hotels-destinationV8', city, { delay: 300 });
@@ -65,6 +67,7 @@ export class AgodaService {
     await this.utilsService.delayRandomTime('slow');
   }
 
+  // 리뷰순으로 정렬
   async sortByReviewCount(page: Page) {
     // 정렬 탭 클릭
     await page.click('.tab-sort-v8');
@@ -78,117 +81,165 @@ export class AgodaService {
     await this.utilsService.delayRandomTime('slow');
   }
 
-  async captureHotelImgs(page: Page, hotelName: string) {
+  // 로그인 페이지 처리
+  async processLoginPage(page: Page) {
+    await this.utilsService.delayRandomTime('quick');
+    await Promise.all([
+      page.waitForNavigation({ waitUntil: 'load' }),
+      page.goBack(),
+    ]);
+    await this.utilsService.delayRandomTime('slow');
+  }
+
+  // 호텔 각 시설별 이미지 캡쳐
+  async captureHotelImgs(hotelPage: Page, hotelName: string) {
+    // 이미지 저장 경로
     const root = path.resolve('.');
     const savePath = path.join(root, 'screenshot', 'tripdotcom');
-    const imgElements = await page.$$('.headAlbum_headAlbum_img__vfjQm');
-    console.log(imgElements);
-    let imgCount = 1;
-    for (const imgElement of imgElements) {
-      console.log(imgElement);
-      if (!fs.existsSync(savePath)) {
-        fs.mkdirSync(savePath, { recursive: true });
-      }
-      const fullPath = path.join(savePath, `${hotelName}_${imgCount}.png`);
-      await imgElement.screenshot({ path: fullPath });
-      imgCount += 1;
+    if (!fs.existsSync(savePath)) {
+      fs.mkdirSync(savePath, { recursive: true });
+    }
+
+    // 이미지 캡쳐
+    const imgSelectors = await hotelPage.$$('.headAlbum_headAlbum_img__vfjQm');
+    for (let i = 0; i < imgSelectors.length - 1; i += 1) {
+      // 이미지 선택기 클릭
+      await imgSelectors[i].click();
+      await this.utilsService.delayRandomTime('slow');
+      // 선택된 이미지 캡쳐
+      const imgElement = await hotelPage.waitForSelector(
+        '.EbPLUEOH7RimYsS10M9X > .VouSwSHeDhUzR1MmyiCA',
+      );
+      const imgBox = await imgElement.boundingBox();
+      await hotelPage.screenshot({
+        path: path.join(savePath, `${hotelName}_${i + 1}.png`),
+        clip: {
+          x: imgBox.x,
+          y: imgBox.y,
+          width: imgBox.width,
+          height: imgBox.height,
+        },
+      });
+      // 선택된 이미지 닫기
+      await hotelPage.click('.o7kWSgJIe2nzJrtBhUs0');
+      await this.utilsService.delayRandomTime('slow');
     }
   }
 
+  // 호텔 각 상세 정보 가져오기
+  async getHotelDetail(hotelPage: Page, selector: string) {
+    const [_, detail] = await Promise.all([
+      hotelPage.waitForSelector(selector),
+      hotelPage.$eval(
+        selector,
+        (element) => (element as HTMLElement).innerText,
+      ),
+    ]);
+    await this.utilsService.delayRandomTime('quick');
+    return detail;
+  }
+
+  // 호텔 정보 가져오기
   async getHotelInfos(page: Page) {
-    const hotelElements = (await page.$$('.hotel-info')).slice(0, 5);
     const hotelInfos = [];
+
+    const hotelElements = (await page.$$('.hotel-info')).slice(0, 5);
     for (const hotelElement of hotelElements) {
       const [hotelPage] = await Promise.all([
         new Promise<Page>((resolve) => page.once('popup', resolve)),
         hotelElement.click(),
       ]);
-      await this.utilsService.delayRandomTime('quick');
-      // 호텔 명
-      const [_name, name] = await Promise.all([
-        await hotelPage.waitForSelector(
-          '.headInit_headInit-title_nameA__EE_LB',
-        ),
-        await hotelPage.$eval(
-          '.headInit_headInit-title_nameA__EE_LB',
-          (nameElement) => (nameElement as HTMLElement).innerText,
-        ),
+      await this.utilsService.delayRandomTime('slow');
+
+      // 이름
+      const hotelName = await this.getHotelDetail(
+        hotelPage,
+        '.headInit_headInit-title_nameA__EE_LB',
+      );
+
+      // 가격
+      const hotelPrice = await this.getHotelDetail(
+        hotelPage,
+        '.priceBox_priceBox-container_realPrice__4VuNW',
+      );
+
+      // drawer 열기
+      const drawerSelector = await Promise.race([
+        hotelPage
+          .waitForSelector('.headReview_headReviewV2__DDg7J')
+          .then(() => '.headReview_headReviewV2__DDg7J'),
+        hotelPage
+          .waitForSelector('.headReviewNew_reviewSwitch-review_numA__Qv6sO')
+          .then(() => '.headReviewNew_reviewSwitch-review_numA__Qv6sO'),
       ]);
-      // 호텔 가격
-      const [_price, price] = await Promise.all([
-        await hotelPage.waitForSelector(
-          '.priceBox_priceBox-container_realPrice__4VuNW',
-        ),
-        await hotelPage.$eval(
-          '.priceBox_priceBox-container_realPrice__4VuNW',
-          (priceElement) => (priceElement as HTMLElement).innerText,
-        ),
-      ]);
-      /*
+      await hotelPage.click(drawerSelector);
+      await this.utilsService.delayRandomTime('slow');
+
       // 호텔 평점
-      const [_score, score] = await Promise.all([
-        await hotelPage.waitForSelector(
-          '.headReview_headReviewV2-scoreBox_bold__iELuw',
-        ),
-        await hotelPage.$eval(
-          '.headReview_headReviewV2-scoreBox_bold__iELuw',
-          (scoreElement) => (scoreElement as HTMLElement).innerText,
-        ),
-      ]);
+      const hotelScore = await this.getHotelDetail(
+        hotelPage,
+        '.reviewScores_reviewOverallScores-currentScore__v5Qtj',
+      );
+
       // 리뷰 개수
-      const [_reviewCount, reviewCount] = await Promise.all([
-        await hotelPage.waitForSelector(
-          '.headReview_headReviewV2-totalNumText__eLhiz',
-        ),
-        await hotelPage.$eval(
-          '.headReview_headReviewV2-totalNumText__eLhiz',
-          (reviewElement) => (reviewElement as HTMLElement).innerText,
-        ),
-      ]);
+      const hotelReviewCnt = await this.getHotelDetail(
+        hotelPage,
+        '.reviewScores_reviewOverallScores-scoreCount__AkXS5',
+      );
+
       // 요약
-      await hotelPage.click('.headReview_headReviewV2__DDg7J');
-      await this.utilsService.delayRandomTime('quick');
-      const [_summary, summary] = await Promise.all([
-        await hotelPage.waitForSelector('.aiSummary_aiSummary-summary__sWsfB'),
-        await hotelPage.$eval(
-          '.aiSummary_aiSummary-summary__sWsfB',
-          (summaryElement) => (summaryElement as HTMLElement).innerText,
-        ),
-      ]);
-      await hotelPage.click('.drawer_drawerContainer-icon__46_V');
-      */
-      // 호텔 사진
-      await this.captureHotelImgs(hotelPage, name);
+      const hotelSummary = await this.getHotelDetail(
+        hotelPage,
+        '.aiSummary_aiSummary-summary__sWsfB',
+      );
+
+      // drawer 닫기
+      await hotelPage.click('.drawer_drawerContainer-icon__46_Vj');
+      await this.utilsService.delayRandomTime('slow');
+
       const hotelInfo = {
-        name,
-        price,
-        /*
-        score: `${score}점`,
-        reviewCount,
-        summary,
-        */
+        name: hotelName,
+        price: hotelPrice,
+        score: hotelScore,
+        reviewCount: hotelReviewCnt,
+        summary: hotelSummary,
       };
       hotelInfos.push(hotelInfo);
+
+      // 호텔 사진 캡쳐
+      await this.captureHotelImgs(hotelPage, hotelName);
+
       await hotelPage.close();
     }
-    console.log(hotelInfos);
+    return hotelInfos;
   }
+
+  // 호텔 정보 출력하기
+  showHotelInfos(hotelInfos: {
+    name: string;
+    price: string;
+    score: string;
+    reviewCount: string;
+    summary: string;
+  }) {}
 
   async getHotelInfosByCity(city: string) {
     const { browser, page } = await this.puppeteerService.getBrowser();
     // 호텔 검색으로 이동
     await this.goToSearchPage(page);
-    // 검색
+    // 검색 처리
     await this.searchCity(page, city);
     // 리뷰순으로 정렬
     await this.sortByReviewCount(page);
     // 로그인 페이지 처리
     const isLoginPage = await page.$('.ibu_login_online');
     if (isLoginPage) {
-      await page.goBack();
+      await this.processLoginPage(page);
     }
     // 호텔 정보 가져오기
-    await this.getHotelInfos(page);
+    const hotelInfos = await this.getHotelInfos(page);
+    // 호텔 정보 출력하기
+    console.log(hotelInfos);
     // await browser.close();
   }
 }
